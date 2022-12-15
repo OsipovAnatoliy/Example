0b7cbeae26e36c4351dc4d860338a72cda3a18e2
tosip <tosip@mail.ru>
2022-12-15 11:23:41 +0300

    Первый урок программа 1

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int kvadrat = number * number;
+Console.WriteLine(kvadrat); 
