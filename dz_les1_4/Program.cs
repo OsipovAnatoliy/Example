4544f705f4b5f2a79700d2d66327e0f100803fc2
tosip <tosip@mail.ru>
2023-01-09 13:15:55 +0300

    dz_les1_4

---
+++ dz_les1_4/Program.cs
@@ -0,0 +1,9 @@
+﻿double a, b, c, max;
+Console.WriteLine("Введите 3 числа через пробел!");
+Console.Write("a b c: ");
+var s = Console.ReadLine().Split(" ");
+a = Convert.ToDouble(s[0]);
+b = Convert.ToDouble(s[1]);
+c = Convert.ToDouble(s[2]);
+max = Math.Max(a, Math.Max(b, c));
+Console.WriteLine("Максимальное число: "+ max);
