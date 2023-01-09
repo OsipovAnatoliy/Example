a427d28ea5e01ab8bae5b0b1edeca51260f9a412
tosip <tosip@mail.ru>
2023-01-09 13:21:05 +0300

    dz_les1_6

---
+++ dz_les1_6/Program.cs
@@ -0,0 +1,11 @@
+﻿ int i;
+ Console.Write("Введите число : ");
+ i = int.Parse(Console.ReadLine());
+ if (i % 2 == 0)
+    {
+      Console.Write("Введенное число является четным");
+    }
+    else
+    {
+    Console.Write("Введенное число нечетное");
+    }
