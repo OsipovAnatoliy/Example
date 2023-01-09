21b7c419bdeaba1a13594557d47994c079692f3a
tosip <tosip@mail.ru>
2023-01-09 13:40:15 +0300

    dz_les1_8

---
+++ dz_les1_8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+ for (int i = 1; i <= m; i++)
+    {
+     if (i % 2 == 0) 
+      {
+       Console.Write(i + " ");
+      }
+    }
