86e80d12e45205d8d7ef999713136b1cfd821d0c
tosip <tosip@mail.ru>
2023-01-09 11:26:09 +0300

    1

---
+++ DZ_les2/Program.cs
@@ -0,0 +1,3 @@
+﻿int num = new Random().Next(100, 1000);
+int secondNumber = num / 10 % 10;
+Console.WriteLine($"Вторая цифра в числе {num} -> {secondNumber}");
--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите число:");
+﻿Сonsole.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 int kvadrat = number * number;
 Console.WriteLine(kvadrat); 
